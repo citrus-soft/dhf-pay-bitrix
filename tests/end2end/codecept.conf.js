3b93e68aaf307d7c7bfc5c5c5f8bf6980073b22e
Marat Shamshutdinov <m.shamshutdinov@gmail.com>
2022-06-16 22:05:14 +0300

    Тесты оплаты счетов, рефакторинг тестов

--- tests/end2end/codecept.conf.js
+++ tests/end2end/codecept.conf.js
@@ -10,22 +10,48 @@ setCommonPlugins();
 require('dotenv').config({ path: '.env' });
 
 exports.config = {
-  tests: './tests/*_test.js',
+  tests: './tests/*.js',
   output: './output',
+  //translation: "ru-RU",
   helpers: {
+    Rest: {
+      require: './helpers/rest_helper.js',
+    },
     Puppeteer: {
       url: process.env.url || "https://dhfi.s2.citruspro.ru",
       show: true,
       windowSize: '1200x900'
+    },
+    ChaiWrapper : {
+      require: "codeceptjs-chai"
     }
   },
   include: {
     I: './steps_file.js',
-    login: "./lib/login.js",
     modules: "./lib/modules.js",
     paysystems: "./lib/paysystems.js",
+
+    invoiceStep: './steps/invoice.js',
+    smartInvoiceStep: './steps/smartInvoice.js',
+
+    smartInvoicePublicPage: './pages/smartInvoicePublicPage.js',
+    dhfiPaymentPage: './pages/dhfiPaymentPage.js',
+  },
+  plugins: {
+    autoLogin: {
+      enabled: true,
+      saveToFile: true,
+      inject: 'loginAs',
+      users: {
+        admin: {
+          login: I => I.loginAsAdmin(),
+          check: I => I.isLoggedIn('admin'),
+        },
+      },
+    },
   },
   bootstrap: null,
   mocha: {},
   name: 'end2end'
 }
+
